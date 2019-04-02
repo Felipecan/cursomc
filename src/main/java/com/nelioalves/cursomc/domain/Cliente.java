a57739c3de6dce250a38346ef638e32bc5848a4d
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-04-02 17:46:36 -0300

    Continuarei da metade do video 19

---
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc.domain;
+
+import com.nelioalves.cursomc.domain.enums.TipoCliente;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private TipoCliente tipo;
+    
+}
