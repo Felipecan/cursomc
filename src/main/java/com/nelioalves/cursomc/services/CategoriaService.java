ca28e0d8763aca503e479a6b6d7549d52e82ab5d
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-04-02 13:08:14 -0300

    Ajustes no endpont /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,10 @@ public class CategoriaService {
     
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);        
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: "+ Categoria.class.getName()
+            )
+        );
     }
     
 }
