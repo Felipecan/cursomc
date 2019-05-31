f10f3e8b8a856fa35d58026e72a48b6954cfb7d2
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-05-31 11:34:07 -0300

    continua na implementacao do repostory produto

--- src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,19 @@
 package com.nelioalves.cursomc.repositories;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Produto;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+    Page<Produto> search(String nome, List<Categoria> categorias, Pageable pageRequest);
     
 }
